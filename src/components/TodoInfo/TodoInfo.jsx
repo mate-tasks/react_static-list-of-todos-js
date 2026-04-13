ad2f58332aaff0c5c316d08284aac4a083c1250c
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-04-13 14:28:18 -0400

    final

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -7,8 +7,7 @@ export const TodoInfo = ({ todo }) => {
   return (
     <article className={`TodoInfo${completed}`}>
       <h2 className="TodoInfo__title">{todo.title}</h2>
-      {todo.user && 
-      <UserInfo user={todo.user} />}
+      {todo.user && <UserInfo user={todo.user} />}
     </article>
   );
 };
