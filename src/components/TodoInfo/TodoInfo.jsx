f1849fc70730e45d4bf5185c7c6d34766f85a1cc
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-04-13 14:23:08 -0400

    invert collor

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,11 +2,11 @@ import './TodoInfo.scss';
 import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => {
-  const completed = todo.completed ? '' : 'TodoInfo--completed';
+  const completed = todo.completed ? ' TodoInfo--completed' : '';
 
   return (
-    <article className={`TodoInfo ${completed}`}>
-      <h2 className="TodoInfo__title">HTML</h2>
+    <article className={`TodoInfo${completed}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
       <UserInfo user={todo.user} />
     </article>
   );
